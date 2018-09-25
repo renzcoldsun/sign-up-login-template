49a43f9a9f3fec176ad40b9fc4c1bc56f7ae2516
Lawrence CS (VBox Win732) <lawrence@salmingo.com>
2018-09-25 20:24:54 +0800

    Adding a flag to determine which records are not sent

--- .htsend_test.php
+++ .htsend_test.php
@@ -2,12 +2,23 @@
 define('tAccess', TRUE);
 include_once('config.inc.php');
 include_once('functions.inc.php');
-
+$accounts_sent = Array();
 $db = connectDB();
 $sql = "SELECT email FROM dlpclienttable WHERE record_sent=false";
 if($query1 = $db->query($sql)) {
     while($row1 = $query1->fetch_assoc()) {
         $email = $row1["email"];
         sendToServer($email);
+        $accounts_sent[] = $email;
     }
+}
+$db->close();
+
+foreach($accounts_sent as $email) {
+    $db = connectDB();
+    $sql = "UPDATE dlpclienttable SET record_sent=true WHERE email='" . $email . "'";
+    if($query = $db->query($sql)) {
+        echo "Email <" . $email . "> SENT TO SERVER " . websocket_host . ":" . websocket_port;
+    }
+    $db->close();
 }
